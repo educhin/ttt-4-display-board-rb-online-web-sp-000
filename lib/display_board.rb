6837f1c9c863eeeb0dbecf15386280bcad869880
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-11 20:11:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,9 +5,9 @@ def display_board (board)
   separator = "|"
   line = "-----------"
 
-  puts "#{board[0]}#{separator}#{board[1]}#{separator}#{board[2]}"
+  puts "#{board[0]} #{separator} #{board[1]} #{separator} #{board[2]}"
   puts "#{line}"
-  puts "#{board[3]}#{separator}#{board[4]}#{separator}#{board[5]}"
+  puts "#{board[3]} #{separator} #{board[4]} #{separator} #{board[5]}"
   puts "#{line}"
-  puts "#{board[6]}#{separator}#{board[7]}#{separator}#{board[8]}"
+  puts "#{board[6]} #{separator} #{board[7]} #{separator} #{board[8]}"
 end
